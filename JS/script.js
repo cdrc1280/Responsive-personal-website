2b26392ab541c6fd0e62afbff304790371729f95
cdrc1280 <106477484+cdrc1280@users.noreply.github.com>
2023-05-15 18:09:43 +0800

    first commit

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// Typing Animation
+let typed = new Typed(".typing", {
+  strings: ["", "Web designer", "Web Developer", "Front End Designer", "Front End Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
